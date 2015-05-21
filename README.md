c0143d7964e97c84e81a157b049428be26891114
Jonathan Ross <jonross@alum.mit.edu>
2015-05-20 21:44:25 -0400

    Rewrite

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+jwtf (Java, WTF?) - Repeatedly run jstack and filter the
+output for interesting threads.
+
 vhp.py (Visualize HProf) is the beginnings of a script to make
 sense of HPROF output.  At the moment it simply buckets all
 traces through common points and prints % time in each.
--- diffhisto.py
+++ diffhisto.py
@@ -7,45 +7,39 @@ if len(sys.argv) != 3:
     sys.stderr.write("Need 2 histo files\n")
     sys.exit(1)
 
-HISTO = re.compile("^ *\\d+:")
-WHITE = re.compile("[ \t]+")
+Slot = namedtuple("Slot", "count bytes classname")
 
-HistoEntry = namedtuple("HistoEntry", "count bytes classname")
+def parse(line):
+    fields = line.split()[1:]
+    return Slot(int(fields[0]), int(fields[1]), fields[2])
 
-def readhisto(filename):
-    histo = {}
-    for line in open(filename):
-	if HISTO.match(line):
-	    fields = re.split(WHITE, line.strip())[1:]
-	    entry = HistoEntry(int(fields[0]), int(fields[1]), fields[2])
-	    histo[entry.classname] = entry
-    return histo
+def slots(filename):
+    finder = re.compile("^ *\\d+:")
+    return (parse(line) for line in open(filename) if finder.match(line))
 
-h1 = readhisto(sys.argv[1])
-h2 = readhisto(sys.argv[2])
+h1 = {slot.name : slot for slot in slots(sys.argv[1]) }
+h2 = {slot.name : slot for slot in slots(sys.argv[2]) }
 
-def reconcile(h1, h2):
-    for classname in h1:
-	if classname not in h2:
-	    h2[classname] = HistoEntry(0, 0, classname)
+h1.update({name: Slot(0, 0, name) for name in h2 if name not in h1})
+h2.update({name: Slot(0, 0, name) for name in h1 if name not in h2})
 
-reconcile(h1, h2)
-reconcile(h2, h1)
-
-def delta(h1, h2):
-    return HistoEntry(h2.count - h1.count, h2.bytes - h1.bytes, h1.classname)
+def delta(s1, s2):
+    return Slot(s2.count - s1.count, s2.bytes - s1.bytes, s1.classname)
 
 h3 = [delta(h1[classname], h2[classname]) for classname in h1.keys()]
+h3.sort(lambda s1, s2: abs(s1.bytes) - abs(s2.bytes), None, True)
 
-def compare(h1, h2):
-    if h1.bytes != h2.bytes:
-	return h1.bytes - h2.bytes
-    if h1.count != h2.count:
-	return h1.count - h2.count
-    return 0
+total = sum([abs(slot.bytes) for slot in h3])
 
-h3.sort(compare, None, True)
+def pad(value, fmt, width):
+    return (fmt % value).rjust(width)
 
-print "%10s%12s  %s" % ("count", "bytes", "class")
-for entry in h3:
-    print "%10d%12d  %s" % (entry.count, entry.bytes, entry.classname)
+cum = 0
+print "  pct    cum %10s %12s  %s" % ("count", "bytes", "class")
+for slot in h3:
+    cum += abs(slot.bytes)
+    pct = 1.0 * abs(slot.bytes) / total * 100
+    cumpct = 1.0 * cum / total * 100
+    print "%s%%  %s%% %10d %12d  %s" % (
+            pad(pct, "%.1f", 4), pad(cumpct, "%.1f", 4),
+            slot.count, slot.bytes, slot.classname)
